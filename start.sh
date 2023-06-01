f05f8c9af42de17247182ebb7ba1a37f14326f06
88plug <19512127+88plug@users.noreply.github.com>
2023-05-31 17:40:56 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
+# Check if PASS variable is defined
+if [[ -z "$PASS" ]]; then
+  echo "❌ ERROR: PASS variable is not defined"
+  exit 1
+fi
+
+# Check if key.pem file exists
+if [[ ! -f "key.pem" ]]; then
+  echo "❌ ERROR: key.pem file does not exist"
+  exit 1
+fi
+
+# Environment variables setup
 export AKASH_CHAIN_ID=akashnet-2
 export AKASH_NODE=$NODE
 export AKASH_KEYRING_BACKEND=os
@@ -6,13 +21,31 @@ export AKASH_NET=https://raw.githubusercontent.com/ovrclk/net/master/mainnet
 export AKASH_KEY_NAME=default
 export AKASH_SECRET=$PASS
 
-export AKASH_ACCOUNT_ADDRESS=$(akash query provider list -o json --limit 10000 --node $NODE | jq -r '.providers[] | select(.host_uri | contains("'"$PROVIDER"'")) | .owner')
-echo "Found Akash provider address $PROVIDER"
+# Retrieve AKASH_ACCOUNT_ADDRESS
+AKASH_ACCOUNT_ADDRESS=$(akash query provider list -o json --limit 10000 --node $NODE | jq -r '.providers[] | select(.host_uri | contains("'"$PROVIDER"'")) | .owner')
+if [ -z "$AKASH_ACCOUNT_ADDRESS" ]; then
+    echo "❌ Failed to find Akash provider address for $PROVIDER"
+    exit 1
+fi
+echo "🔍 Found Akash provider address $PROVIDER"
 export PROVIDER=$AKASH_ACCOUNT_ADDRESS
-pwd
-echo "Adding password to key-pass.txt"
+
+# Write password to 'key-pass.txt'
+echo "💾 Adding password to key-pass.txt"
 echo "$PASS" > key-pass.txt
-echo "Importing key first"
-(sleep 2s; cat key-pass.txt; cat key-pass.txt; cat key-pass.txt ) | akash keys import default key.pem
-echo "SUCCESS!"
-./loop-payout.sh
+
+# Import key
+echo "🔑 Importing key first"
+if ! (sleep 2s; cat key-pass.txt; cat key-pass.txt; cat key-pass.txt) | akash keys import default key.pem; then
+    echo "❌ Failed to import key"
+    exit 1
+fi
+
+# Display success message
+echo "✅ SUCCESS!"
+
+# Execute loop-payout.sh script
+if ! ./loop-payout.sh; then
+    echo "❌ Failed to execute loop-payout.sh"
+    exit 1
+fi
